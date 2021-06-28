527eda0031cd388c8540863e984181c602459865
admin <tagiyev.z@gmail.com>
2021-06-27 23:20:50 -0700

    added 1 commit

--- src/git3.java
+++ src/git3.java
@@ -2,5 +2,6 @@ public class git3 {
     public static void main(String[] args) {
         System.out.println("Hello again");
          System.out.println("Hello again");
+        System.out.println("Hello again");
     }
 }
